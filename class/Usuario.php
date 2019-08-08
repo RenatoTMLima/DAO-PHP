e38a6e86c6fa41c6f92f0a28c2efd61c606cb308
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2019-08-08 16:35:14 -0300

    Inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -28,12 +28,66 @@ class Usuario{
         $res = $sql->select("SELECT * FROM tb_usuarios WHERE id_usuario = :ID", array(":ID"=>$id));
 
         if (isset($res[0])){
-            $row = $res[0];
-            $this->setIdUsuario($row['id_usuario']);
-            $this->setDeslogin($row['des_login']);
-            $this->setDessenha($row['des_senha']);
-            $this->setDtcadastro(new DateTime($row['dt_cadastro']));
+            $this->setData($res[0]);
         }
+    }
+
+    public static function getList(){
+        $sql = new SQL();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY des_login");
+    }
+
+    public static function search($login){
+        $sql = new SQL();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE des_login LIKE :SEARCH ORDER BY des_login", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public function login($login, $pass){
+        $sql = new SQL();
+
+        $res = $sql->select("SELECT * FROM tb_usuarios WHERE des_login = :LOGIN AND des_senha = :PASS", array(
+            ":LOGIN"=>$login, 
+            ":PASS"=>$pass));
+
+        if (isset($res[0])){
+
+            $this->setData($res[0]);
+        } else {
+            throw new Exception("Login e/ou senha inválidos.");
+        }
+    }
+
+    public function setData($data){
+        $this->setIdUsuario($data['id_usuario']);
+        $this->setDeslogin($data['des_login']);
+        $this->setDessenha($data['des_senha']);
+        $this->setDtcadastro(new DateTime($data['dt_cadastro']));
+    }
+
+    public function insert($login, $senha){
+        $sql = new SQL();
+        $res = $sql->query("INSERT INTO tb_usuarios (des_login, des_senha) VALUES (:LOGIN, :PASS)", array(
+            ":LOGIN"=>$login,
+            ":PASS"=>$senha
+        ));
+
+        return $this->search($login);
+    }
+
+    public function update($login, $pass){
+        $this->setDeslogin($login);
+        $this->setDessenha($pass);
+
+        $sql = new SQL();
+        $sql->query("UPDATE tb_usuarios SET des_login = :LOGIN, des_senha = :PASS WHERE id_usuario = :ID", array(
+            ":LOGIN"=>$login,
+            ":PASS"=>$pass,
+            ":ID"=>$this->getIdUsuario()
+        ));
     }
 
     public function __toString()
--- index.php
+++ index.php
@@ -2,8 +2,39 @@
 
 require_once("config.php");
 
-$us = new Usuario();
+//Carrega um usuario
+// $us = new Usuario();
 
-$us->loadById(3);
+// $us->loadById(3);
+
+// echo $us;
+
+//Carrega uma lista de usuarios
+// $lista = Usuario::getList();
+
+// print_r($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("ena");
+
+// print_r($search);
+
+//Carrega um usuario usando login e senha
+// $usuario = new Usuario();
+
+// $usuario->login("Renato", 12345);
+
+// echo $usuario;
+
+//Executando o novo metodo insert
+// $us = new Usuario();
+
+// print_r($us->insert("Tainara", "13579"));
+
+
+//Utilizando o novo metodo update
+$us = new Usuario();
+$us->loadById(5);
+$us->update("Doutor", "ksjdfvblsj");
 
 echo $us;
